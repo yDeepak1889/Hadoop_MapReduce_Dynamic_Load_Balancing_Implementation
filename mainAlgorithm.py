3afb8798a96375453a6ead85960ba6b46026efda
stark03 <tusharmurarka@gmail.com>
2018-04-14 00:09:35 +0530

    Added Object Oriented structure to file

--- mainAlgorithm.py
+++ mainAlgorithm.py
@@ -1,113 +1,101 @@
 from resourceReportClient import *
 
-resources = {}
-resources = getResourceStatusOfDataNodes()
+class mainAlgorithm(object):
+
+	def __init__(self, resources):
+
+		self.resources = resources
+		self.List_cpu = {}
+		self.List_ram = {}
+		self.S_node   = {}
+		self.U_node   = {}
+		self.P_node   = {}
+		self.R_node   = {}
+		self.R_hit    = {}
+		self.R_cluster= 0
+		self.R_max    = 0
+		self.minRam   = 10000000000000000
+		self.minCPU   = 10000000000000000
 
 
+	def parametersCalc(self):
 
-List_cpu = {}
-List_ram = {}
-S_node   = {}
-U_node   = {}
-P_node   = {}
-R_node   = {}
-R_hit    = {}
-R_cluster= 0
-R_max    = 0
+		'''Function calculates basic parameters which are to be used by otherParams()'''
 
-minRam   = 10000000000000
-minCPU   = 10000000000000
+		for i in self.resources.keys() :
+
+			cpu = self.resources[i]['cpu']
+			freq  = cpu[0]
+			cores = cpu[1]
 
 
-def parametersCalc():
+			if(cores == 1):
+				P_cpu = freq
+			else :
+				P_cpu = freq*cores*0.8
 
-	global minRam
-	global minCPU
+			if P_cpu < self.minCPU:
+				self.minCPU = P_cpu
 
-	for i in resources.keys() :
+			self.List_cpu[i] = P_cpu
 
-		cpu = resources[i]['cpu']
-		freq  = cpu[0]
-		cores = cpu[1]
+			#Total RAM
+			P_ram = self.resources[i]['ram'][0]
+			self.List_ram[i] = P_ram
 
-	
-		if(cores == 1):
-			P_cpu = freq
-		else :
-			P_cpu = freq*cores*0.8 
+			if P_ram < self.minRam:
+				self.minRam = P_ram
 
-		if P_cpu < minCPU:
-			minCPU = P_cpu
+			#Storage capacity self.S_node , Used Capacity self.U_node
+			S = self.resources[i]['disk'][0]
+			self.S_node[i] = S
 
-		List_cpu[i] = P_cpu
-
-		#Total RAM
-		P_ram = resources[i]['ram'][0]
-		List_ram[i] = P_ram
-
-		if P_ram < minRam:
-			minRam = P_ram
-
-		#Storage capacity S_node , Used Capacity U_node
-		S = resources[i]['disk'][0]
-		S_node[i] = S
-
-		U = resources[i]['disk'][1]
-		U_node[i] = U
+			U = self.resources[i]['disk'][1]
+			self.U_node[i] = U
 
 
 
 
-#typeOfTask : 1 for memory bound, 0 for IO bound
-#gama : Lies between 0 and 1, decides the max load rate of cluster, 0.8 generally
+	#typeOfTask : 1 for memory bound, 0 for IO bound
+	#gama : Lies between 0 and 1, decides the max load rate of cluster, 0.8 generally
+	def otherParams(self, typeOfTask, gama):
 
-def otherParams(typeOfTask, gama):
-	
-	if(typeOfTask == 1):
-		alpha = 0.8
-		beta  = 0.2
+		'''Function calculates all parameters required for main algorithm : R_hit, R_cluster, ....'''
 
-	else:
-		alpha = 0.2
-		beta  = 0.8
+		if(typeOfTask == 1):
+			alpha = 0.8
+			beta  = 0.2
 
-	max_pBYu = -1
-	U_cluster = 0
-	S_cluster = 0
+		else:
+			alpha = 0.2
+			beta  = 0.8
 
-	global R_cluster
-	global R_max
+		max_pBYu = -1
+		U_cluster = 0
+		S_cluster = 0
 
-	for i in S_node.keys():
+		for i in self.S_node.keys():
 
-		Pi = alpha*(List_cpu[i]/minCPU) + beta*(List_ram[i]/minRam)
-		P_node[i] = Pi
+			Pi = alpha*(self.List_cpu[i]/self.minCPU) + beta*(self.List_ram[i]/self.minRam)
+			self.P_node[i] = Pi
 
-		t = Pi/U_node[i]
+			t = Pi/self.U_node[i]
 
-		if max_pBYu < t:
-			max_pBYu = t
+			if max_pBYu < t:
+				max_pBYu = t
 
-		Ri = (U_node[i]/S_node[i])*100
-		R_node[i] = Ri
+			Ri = (self.U_node[i]/self.S_node[i])*100
+			self.R_node[i] = Ri
 
-		U_cluster += U_node[i]
-		S_cluster += S_node[i]
+			U_cluster += self.U_node[i]
+			S_cluster += self.S_node[i]
 
 
-	R_cluster = (U_cluster/S_cluster)*100       #Load Ratio of the cluster
-	R_max =  (gama + (1-gama)*R_cluster)*100 	#Maximum Load rate of the cluster
+		self.R_cluster = (U_cluster/S_cluster)*100       #Load Ratio of the cluster
+		self.R_max =  (gama + (1-gama)*self.R_cluster)*100 	#Maximum Load rate of the cluster
 
+		#Calculating self.R_hit for each node
+		for key in self.P_node.keys():
 
-	#Calculating R_hit for each node
-
-	for key in P_node.keys():
-
-		Rhi = (P_node[key]/U_node[key])/(max_pBYu)
-		R_hit[key] = Rhi
-
-
-parametersCalc()
-otherParams(1, 0.8)
-
-
+			Rhi = (self.P_node[key]/self.U_node[key])/(max_pBYu)
+			self.R_hit[key] = Rhi
