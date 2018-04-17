f12e6d64b009f21071c7c7ee493bb5d982667aea
Himanshu <himu5493@gmail.com>
2018-04-18 03:02:05 +0530

    scraper complete

--- webScrape.py
+++ webScrape.py
@@ -1,4 +1,6 @@
-
+#!/usr/bin/python3i
+# ret stores a dictionary {index:[length,noOfLiveNode,listOfIp]}
+# listOfIp [local ip, dest ip 1,dest ip 2...]
 import urllib.request
 import re
 
@@ -8,20 +10,20 @@ class webScraping(object):
         self.data = ""
         
     def readData(self, url):
-
+        ret ={}
         req = urllib.request.Request(url)
         resp = urllib.request.urlopen(req)
         self.data = resp.read().decode("utf-8") 
-        regex = r"([0-9])\..*len=([0-9]+).*Live_repl=([0-9]+).*Storage\[([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*\]\]"
+        regex = r"([0-9])\..*len=([0-9]+).*Live_repl=([0-9]+).*\]\]"
         matches = re.finditer(regex,self.data)
         for matchNum , match in enumerate(matches):
-            matchNum = matchNum + 1
-            print(match.group())
-            for groupNum in range(0, len(match.groups())):
-                        groupNum = groupNum + 1
-                        print(match.group(groupNum))
-c = webScraping()
-c.readData('http://172.20.33.93:50070/fsck?ugi=hadoop&files=1&blocks=1&locations=1&path=%2Fuser%2Fhadoop%2Fbooks%2Falice.txt')
-
-
+            x = match.groups()
+            regex =  r"([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)"
+            matches_ip = re.finditer(regex,match.group())
+            ip_list = []
+            for ip_c,ip_s in enumerate(matches_ip):
+                y = ip_s.groups()
+                ip_list.append(y[0])
+            ret[x[0]]=[x[1],x[2],ip_list]
+        return ret
 
