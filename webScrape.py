8eb844d69d56a5674d950a2d7805807bb3091f42
stark03 <tusharmurarka@gmail.com>
2018-04-17 23:38:07 +0530

    Added webScraping file

--- webScrape.py
+++ webScrape.py
@@ -2,9 +2,19 @@
 import urllib.request
 import re
 
-url = 'http://172.20.33.93:50070/fsck?ugi=hadoop&files=1&blocks=1&locations=1&path=%2Fuser%2Fhadoop%2Fbooks%2Falice.txt'
+class webScraping(object):
 
-req = urllib.request.Request(url)
-resp = urllib.request.urlopen(req)
-respData = resp.read()
-print(str(respData))
+    def __init__(self):
+        self.data = ""
+
+    def readData(self, url):
+
+        req = urllib.request.Request(url)
+        resp = urllib.request.urlopen(req)
+        self.data = resp.read()
+        print(self.data)
+
+
+c = webScraping()
+c.readData('http://172.20.33.93:50070/fsck?ugi=hadoop&files=1&blocks=1&locations=1&path=%2Fuser%2Fhadoop%2Fbooks%2Falice.txt')
+
