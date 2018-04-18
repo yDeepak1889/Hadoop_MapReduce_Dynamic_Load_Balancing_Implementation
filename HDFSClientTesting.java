470134cfff5abc3f9a1aa0bea95560871342346b
yDeepak1889 <iit2015124@iiita.ac.in>
2018-04-18 23:08:53 +0530

    Read with seek done

--- HDFSClientTesting.java
+++ HDFSClientTesting.java
@@ -22,31 +22,42 @@ public class HDFSClientTesting extends Configured implements Tool {
     
     public int run(String[] args) throws Exception {
        
-		Configuration conf = getConf();
+	Configuration conf = getConf();
 
-		System.out.println("configured filesystem = " + conf.get(FS_PARAM_NAME));
+	System.out.println("configured filesystem = " + conf.get(FS_PARAM_NAME));
 
-		FileSystem fs = FileSystem.get(conf);
+	FileSystem fs = FileSystem.get(conf);
 
-		Path dataset = new Path(fs.getHomeDirectory(), "books/test1.txt");
+	Path dataset = new Path(fs.getHomeDirectory(), "books/alice.txt");
 
-		if (!fs.exists(dataset)) {
-			System.err.println("output path doesn't exist");
-			return 1;
-		}
+	if (!fs.exists(dataset)) {
+		System.err.println("output path doesn't exist");
+		return 1;
+	}
 
-		FSDataInputStream in = fs.open(dataset);
-		
-		System.out.println("-----"+in.available());
-    	byte [] out = new byte[100];
-    	in.readFully(out, 0, 10);
-    	System.out.println(Arrays.toString(out));
-    	fs.close();
-		
-		//long offset = 10;
-		//in.seek(offset);
-		
-		
+	FSDataInputStream in = fs.open(dataset);
+	int len = in.available();
+	System.out.println("-----"+ len);
+    	
+	byte [] out = new byte[len];
+	
+	int numBytes;
+
+    	in.readFully(170000, out, 0, len-170000);
+	/*
+	String temp;	
+
+	 while ((numBytes = in.read(out)) > 0) {
+		temp = new String (out, "UTF-8");
+		System.out.println(temp);	                
+         }
+
+	*/
+	String outputStr = new String (out, "UTF-8");
+    	System.out.println(outputStr);
+    	out = null;
+	fs.close();
+	
     	return 0;
     }
 
